508db4135bf321bf3eaedda4d07897deaa984797
jason <jason.addie@gmail.com>
2013-06-28 14:34:22 +0400

    fixed name of git repo

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ set :user, "timeline-staging" # name of user on server
 set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
-set :github_repo_name, "Election-Timeline" # name of git hub repo
+set :github_repo_name, "Timeline" # name of git hub repo
 set :git_branch_name, "feradi" # name of branch to deploy
 set :rails_env, "staging" # name of environment: production, staging, ...
 ##################################
