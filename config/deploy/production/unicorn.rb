bce48e47b76f6e58d2bf35fd3a2e5bfdf8ab1c70
jason <jason.addie@gmail.com>
2013-07-09 12:10:49 +0400

    added production deploy info

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,9 +1,9 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap/Bootstrap-Starter/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
-port_num = 8091 # must be a unique port number for this application
+root = "/home/timeline/Timeline/current" # path to application current folder
+sock_name = "unicorn_Timeline" # must be unique name with no '.'
+port_num = 8114 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
