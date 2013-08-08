51fbbd6bb2b5e63e2822dc5d87e8ee9ed6f9cb63
jason <jason.addie@gmail.com>
2013-08-08 17:11:10 +0400

    fixed tag display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -78,15 +78,19 @@ logger.debug "////////////////////////// BROWSER NOT SUPPORTED"
 
     if categories.present? || tags.present?
       x << "<div class='story_cat_tags fluid-row clear'>"
+=begin
       if categories.present?
         x << "<div class='span6'>"
         x << "<strong>#{I18n.t('categories.category')}:</strong> "
         x << categories.sort_by{|y| y[:name]}.map{|x| view_context.link_to(x[:name], root_path(:category => x[:permalink], :locale => I18n.locale))}.join(", ")
         x << "</div>"
       end
+=end      
       if tags.present?
-        x << "<div class='span6'>"
-        x << "<strong>#{I18n.t('categories.tag')}:</strong> "
+#        x << "<div class='span6'>"
+        x << "<div class='span12'>"
+#        x << "<strong>#{I18n.t('categories.tag')}:</strong> "
+        x << "#{I18n.t('categories.tag')}: "
         x << tags.sort_by{|y| y[:name]}.map{|x| view_context.link_to(x[:name], url_for(params.merge(:tag => x[:permalink], :locale => I18n.locale)))}.join(", ")
         x << "</div>"
       end
