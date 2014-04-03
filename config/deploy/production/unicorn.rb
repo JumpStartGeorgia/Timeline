3ba5b5e50a9acb5610785da754e1623c53813ffd
jason <jason.addie@gmail.com>
2014-04-03 12:12:18 +0400

    updated production deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,9 +1,9 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/etimeline/Election-Timeline/current" # path to application current folder
-sock_name = "unicorn_Election-Timeline" # must be unique name with no '.'
-port_num = 8110 # must be a unique port number for this application
+root = "/home/gudiashvili/Gudiashvili-Timeline/current" # path to application current folder
+sock_name = "unicorn_Gudiashvili-Timeline" # must be unique name with no '.'
+port_num = 8124 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
