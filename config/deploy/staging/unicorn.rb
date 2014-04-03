72a458dd270742b748356c8e3b2272f4136467bb
jason <jason.addie@gmail.com>
2014-04-03 11:59:44 +0400

    updated staging deploy

--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -1,9 +1,9 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/election-timeline-staging/Election-Timeline-Staging/current" # path to application current folder
-sock_name = "unicorn_Election-Timeline-Staging" # must be unique name with no '.'
-port_num = 8110 # must be a unique port number for this application
+root = "/home/gudiashvili-staging/Gudiashvili-Timeline-Staging/current" # path to application current folder
+sock_name = "unicorn_Gudiashvili-Timeline-Staging" # must be unique name with no '.'
+port_num = 8124 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
