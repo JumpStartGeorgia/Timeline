725023216e0267f9cb330999ab2621bec5e025a6
jason <jason.addie@gmail.com>
2014-04-22 12:25:28 +0400

    updated about text and spacing

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -221,6 +221,28 @@ $(document).ready(function() {
       $('#panorama').reel($('#panorama').data());
     }
 
+
+    // as the window scrolls, hide the arrow
+    $(window).scroll( function(){
+
+      //get scroll position
+      var topWindow = $(window).scrollTop();
+      //multiply by 1.5 so the arrow will become transparent half-way up the page
+      var topWindow = topWindow * 1.5;
+      
+      //get height of window
+      var windowHeight = $(window).height();
+          
+      //set position as percentage of how far the user has scrolled 
+      var position = topWindow / windowHeight;
+      //invert the percentage
+      position = 1 - position;
+
+      //define arrow opacity as based on how far up the page the user has scrolled
+      //no scrolling = 1, half-way up the page = 0
+      $('.arrow-wrap').css('opacity', position);
+
+    });
     
     // create index of all items in timeline for searching
     search_index = lunr(function () {
