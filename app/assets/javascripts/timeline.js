e9d55e42f9fc4ce04cf228e44b3253a978fd0c94
nwshane <django09@gmail.com>
2015-12-01 14:00:43 +0400

    Added selector class to arrow wrap for JS to transition its opacity.

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -258,7 +258,7 @@ $(document).ready(function() {
 
       //define arrow opacity as based on how far up the page the user has scrolled
       //no scrolling = 1, half-way up the page = 0
-      $('.arrow-wrap').css('opacity', position);
+      $('.js-transition-arrow-wrap-opacity').css('opacity', position);
 
     });
 
