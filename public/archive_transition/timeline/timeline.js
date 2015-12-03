7e099fec13aa291138fbaa7ca1c2dfa75d919a74
nwshane <django09@gmail.com>
2015-12-03 13:12:49 +0400

    Removed panorama code and is_home_page gon variable use in archive transition timeline JS.

--- public/archive_transition/timeline/timeline.js
+++ public/archive_transition/timeline/timeline.js
@@ -192,73 +192,27 @@ $(socials.children('.st_sharethis_custom')[0]).attr('st_url', url).attr('st_titl
 
 $(document).ready(function() {
 
-  if (gon.is_home_page){
-    // if screen is small, switch out the background image
-    if ($(window).width() < 427){
-      $('#panorama').prop('src', '/assets/bg_mobile_small.jpg');
-    }else if ($(window).width() < 623){
-      $('#panorama').prop('src', '/assets/bg_mobile_big.jpg');
-    }else{
-      $('#panorama').prop('src', '/assets/bg_static.jpg');
-    }
-
-
-
-    // adjust image to start with statue in middle of circle
-    // formula: (frames to middle) - ( (1/2 width of screen) / (frame width) )
-    var img_ratio = $(window).height() / $('#panorama').data('orig-height');
-    var new_width = $('#panorama').data('orig-width') * img_ratio;
-    var frames_middle = 1086;
-    var frame_width = new_width / $('#panorama').data('frames');
-    var new_f = frames_middle - ($(window).width()/2/frame_width);
-
-    // make image fit height of screen
-    var panoramaResize = function()
-    {
-      var ratio = $(window).height() / $('#panorama').data('orig-height');
-      var new_height = $('#panorama').data('orig-height') * ratio;
-      var new_width = $('#panorama').data('orig-width') * ratio;
-      $('#panorama').css('height', new_height).css('background-size',  new_width + 'px ' + new_height + 'px');
-//      $('#panorama').css('height', new_height).css('background-size',  '4344px ' + new_height + 'px');
-//      $('#panorama').css('width', new_width);
-      $('#panorama').data('stitched', new_width);
-      $('#panorama-reel').css('height', $(window).height());
-    }
-    panoramaResize();
-
-	  // resize the panorama timeline when the screen changes
-    window.onresize = function()
-    {
-      panoramaResize();
-      $('#timeline-embed').css('height', String($(window).height()-$('.js-get-footer-height').height()) + "px");
-    }
-
-    // if this is not a small screen, turn on the scrolling panaorama
-    if ($(window).width() > 978){
-      $('#panorama').data('frame', new_f);
-      $('#panorama').reel($('#panorama').data());
-    }
-
-
-    // as the window scrolls, hide the arrow
-    $(window).scroll( function(){
-
-      //get scroll position
-      var topWindow = $(window).scrollTop();
-      //multiply by 1.5 so the arrow will become transparent half-way up the page
-      var topWindow = topWindow * 1.5;
-
-      //get height of window
-      var windowHeight = $(window).height();
-
-      //set position as percentage of how far the user has scrolled
-      var position = topWindow / windowHeight;
-      //invert the percentage
-      position = 1 - position;
-
-    });
-
+  window.onresize = function() {
+    $('#timeline-embed').css('height', String($(window).height()-$('.js-get-footer-height').height()) + "px");
   }
+
+  // as the window scrolls, hide the arrow
+  $(window).scroll( function(){
+
+    //get scroll position
+    var topWindow = $(window).scrollTop();
+    //multiply by 1.5 so the arrow will become transparent half-way up the page
+    var topWindow = topWindow * 1.5;
+
+    //get height of window
+    var windowHeight = $(window).height();
+
+    //set position as percentage of how far the user has scrolled
+    var position = topWindow / windowHeight;
+    //invert the percentage
+    position = 1 - position;
+
+  });
 
   if (gon.show_timeline){
     // clone the json data so searching can search through the original
