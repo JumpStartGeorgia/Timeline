09251d479dc5bb2ea4cf198789f95d8b2ddb75b8
nwshane <django09@gmail.com>
2016-03-18 12:06:19 +0400

    Factored out show_about and hide_about functions.

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,9 +1,13 @@
-function setup_about() {
-  $('.js-show-about-box').click(function() {
-    $('.js-make-about-showable-hideable').removeClass('is-hidden');
-  });
+function show_about() {
+  $('.js-make-about-showable-hideable').removeClass('is-hidden');
+}
 
-  $('.js-hide-about-box').click(function() {
-    $('.js-make-about-showable-hideable').addClass('is-hidden');
-  });
+function hide_about() {
+  $('.js-make-about-showable-hideable').addClass('is-hidden');
+}
+
+function setup_about() {
+  $('.js-show-about-box').click(show_about);
+
+  $('.js-hide-about-box').click(hide_about);
 }
