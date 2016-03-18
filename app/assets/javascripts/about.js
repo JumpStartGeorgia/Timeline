199f37d11fda4aa7ec3911911b94342fd057ca1f
nwshane <django09@gmail.com>
2016-03-18 12:10:28 +0400

    Added escape key event to close about window.

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -10,4 +10,8 @@ function setup_about() {
   $('.js-show-about-box').click(show_about);
 
   $('.js-hide-about-box').click(hide_about);
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) hide_about(); // Escape key
+  });
 }
