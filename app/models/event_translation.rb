936311feed29210592d3a0dd56eee8b9e1c77497
jason <jason.addie@gmail.com>
2016-09-26 15:48:46 +0400

    fix load media file

--- app/models/event_translation.rb
+++ app/models/event_translation.rb
@@ -3,12 +3,12 @@ class EventTranslation < ActiveRecord::Base
 	require 'utf8_converter'
 
 	belongs_to :event
-	has_attached_file :media_img, 
+	has_attached_file :media_img,
     :url => "/system/media_img/:id/:style/:filename",
     :styles => {
         :'processed' => {:geometry => "800>"}
     },
-    :convert_options => { 
+    :convert_options => {
       :'processed' => '-quality 85'
     }
 
@@ -30,12 +30,12 @@ class EventTranslation < ActiveRecord::Base
 
   def required_data_provided?
     provided = false
-    
+
     provided = self.headline.present?
-    
+
     return provided
   end
-  
+
   # only headline is required, but go ahead and add the rest of content
   def add_required_data(obj)
     self.headline = obj.headline if self.headline.blank?
@@ -46,17 +46,19 @@ class EventTranslation < ActiveRecord::Base
   end
 
 
-  # if the url in the media field is for an image
+
+# if the url in the media field is for an image
   # download it and save to media_img
   def create_media_file
     if self.media.present? && ((self.media != self.media_original) || (!self.media_img_verified && self.media_img_file_name.blank?))
+      media_url = self.media.dup
       begin
-        file = open(self.media)
+        file = open(media_url)
         if file.present?
           case file.content_type
           when "image/png", "image/gif", "image/jpeg", "image/pjpeg"
             # create the file name that paperclip will read in
-            extension = File.extname(URI.parse(self.media).path)
+            extension = File.extname(URI.parse(media_url).path)
             # if extension is not in url, us the content type
             # if extension is php (e.g., http://www.tbilisi.gov.ge/functions/img.php?src_jpg=../album/1297_5992_668123.jpg&im_new_w=500)
             # - default to content type to get true content type
@@ -70,11 +72,23 @@ class EventTranslation < ActiveRecord::Base
             self.media_img = file
           else
             puts "!!!!!!!!!!!!!!!!!!!!!"
-            puts self.media
+            puts media_url
             puts "the file is not an image"
             puts "!!!!!!!!!!!!!!!!!!!!!"
           end
           self.media_img_verified = true
+        end
+      rescue RuntimeError => error
+        # example: redirection forbidden: http://bit.ly/gSarwN -> https://github.com/jugyo/termtter/commit/6e5fa4455a5117fb6c10bdf82bae52cfcf57a91f
+        if error.message =~ /^redirection forbidden/
+          puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+          puts error.message
+          puts "trying again with the following url"
+          puts error.message.split(/\s+/).last
+          puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+          media_url = error.message.split(/\s+/).last
+          retry
         end
       rescue OpenURI::HTTPError => the_error
         self.media_img_verified = true
@@ -85,6 +99,46 @@ class EventTranslation < ActiveRecord::Base
       self.media_img = nil
     end
   end
+
+  # # if the url in the media field is for an image
+  # # download it and save to media_img
+  # def create_media_file
+  #   if self.media.present? && ((self.media != self.media_original) || (!self.media_img_verified && self.media_img_file_name.blank?))
+  #     begin
+  #       file = open(self.media)
+  #       if file.present?
+  #         case file.content_type
+  #         when "image/png", "image/gif", "image/jpeg", "image/pjpeg"
+  #           # create the file name that paperclip will read in
+  #           extension = File.extname(URI.parse(self.media).path)
+  #           # if extension is not in url, us the content type
+  #           # if extension is php (e.g., http://www.tbilisi.gov.ge/functions/img.php?src_jpg=../album/1297_5992_668123.jpg&im_new_w=500)
+  #           # - default to content type to get true content type
+  #           extension = "." + file.content_type.split('/')[1] if !extension.present? || extension == '.php'
+  #           name = "media_img"
+  #           name = Utf8Converter.generate_permalink(self.headline) if self.headline.present?
+
+  #           file.define_singleton_method(:original_filename) do
+  #             "#{name}#{extension}"
+  #           end
+  #           self.media_img = file
+  #         else
+  #           puts "!!!!!!!!!!!!!!!!!!!!!"
+  #           puts self.media
+  #           puts "the file is not an image"
+  #           puts "!!!!!!!!!!!!!!!!!!!!!"
+  #         end
+  #         self.media_img_verified = true
+  #       end
+  #     rescue OpenURI::HTTPError => the_error
+  #       self.media_img_verified = true
+  #     end
+  #   # if media is no longer present, delete image file
+  #   elsif !self.media.present? && self.media != self.media_original
+  #     self.media_img_verified = false
+  #     self.media_img = nil
+  #   end
+  # end
 
 
   def media_url
